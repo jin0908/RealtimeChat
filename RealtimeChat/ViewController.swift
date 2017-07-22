7c3a9f638d2ca0a4d11c3fdca912d3e0570f2946
Hyeongjin Um <hyeongjin0908@gmail.com>
2017-07-22 00:04:04 -0400

    Initial Commit

---
+++ RealtimeChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealtimeChat
+//
+//  Created by Hyeongjin Um on 22/07/2017.
+//  Copyright © 2017 Hyeongjin Um. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
